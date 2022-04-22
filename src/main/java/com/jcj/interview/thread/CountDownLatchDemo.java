d216aacd08e4f17fc1cc19e626f76cc6d0ebe207
jcj6085 <jcj6085@qq.com>
2022-04-22 15:38:27 +0800

    new thread pool

--- src/main/java/com/jcj/interview/thread/CountDownLatchDemo.java
+++ src/main/java/com/jcj/interview/thread/CountDownLatchDemo.java
@@ -3,15 +3,19 @@ package com.jcj.interview.thread;
 import cn.hutool.core.thread.ThreadUtil;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CountDownLatchDemo {
 
     private static final int SIZE = 10;
 
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(SIZE);
         for (int i = 1; i <= SIZE; i++) {
-            ThreadUtil.execute(() -> {
+            executorService.execute(() -> {
                 System.out.println(Thread.currentThread().getName() + "\t 上完自习，离开教室");
                 countDownLatch.countDown();
             });
